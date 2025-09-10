f33f2c4e892ce21792e43edd14473f69cb1ac621
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-10 18:47:37 +0530

    feat: implement DELETE endpoint for removing user messages with authentication

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,53 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/Users";
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageid: string } }
+) {
+  const messageId = params.messageid;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const _user: User = session?.user as User;
+
+  if (!session || !_user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updatedResult = await UserModel.updateOne(
+      { _id: _user._id },
+      { $pull: { message: { _id: messageId } } }
+    );
+
+    if (updatedResult.modifiedCount === 0) {
+      return Response.json(
+        { mesage: "Message not found or already deleted", success: false },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      { message: "Message deleted", success: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting message", error);
+    return Response.json(
+      {
+        message: "Error in deleting Message",
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
